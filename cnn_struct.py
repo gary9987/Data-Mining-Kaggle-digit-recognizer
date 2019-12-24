d710ec6a555ab2399582d7be95c312a727afed15
Gary <s310068@shsh.tw>
2019-12-25 00:33:53 +0800

    mod dir struct and code

--- cnn_struct.py
+++ cnn_struct.py
@@ -1,3 +1,4 @@
+# %% [code]
 import numpy as np
 import pandas as pd
 
@@ -11,8 +12,8 @@ from keras.layers.normalization import BatchNormalization
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 import csv
 
-train = pd.read_csv("./datas/train.csv")
-test = pd.read_csv("./datas/test.csv")
+train = pd.read_csv("../input/digit-recognizer/train.csv")
+test = pd.read_csv("../input/digit-recognizer/test.csv")
 
 print(train.head())
 
@@ -37,7 +38,6 @@ test = test.values.reshape(len(test),28,28, 1)
 print(train_x.shape, test.shape) #(42000, 28, 28, 1) (28000, 28, 28, 1)
 
 
-
 #creat CNN model
 print('Creating CNN model...')
 tensor_in = Input((28, 28, 1))
@@ -48,7 +48,6 @@ tensor_out = MaxPooling2D(pool_size=(2, 2))(tensor_out)
 tensor_out = Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu')(tensor_out)
 tensor_out = Conv2D(filters=64, kernel_size=(3, 3), activation='relu')(tensor_out)
 tensor_out = MaxPooling2D((2,2), padding='same')(tensor_out)
-'''
 tensor_out = Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu')(tensor_out)
 tensor_out = Conv2D(filters=128, kernel_size=(3, 3), activation='relu')(tensor_out)
 tensor_out = BatchNormalization(axis=1)(tensor_out)
@@ -59,7 +58,7 @@ tensor_out = MaxPooling2D((2,2), padding='same')(tensor_out)
 tensor_out = Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(tensor_out)
 tensor_out = BatchNormalization(axis=1)(tensor_out)
 tensor_out = MaxPooling2D((2,2), padding='same')(tensor_out)
-'''
+
 tensor_out = Flatten()(tensor_out)
 tensor_out = Dropout(0.5)(tensor_out)
 
@@ -75,16 +74,15 @@ train_y = np_utils.to_categorical(train_y)
 
 from sklearn.model_selection import train_test_split
 s_train_x, s_test_x, s_train_y, s_test_y = train_test_split(train_x, train_y, test_size=0.20, random_state=7)
-model.fit(s_train_x, s_train_y, validation_data=(s_test_x, s_test_y), epochs=40, batch_size=50, verbose=2)
+mhistory = model.fit(s_train_x, s_train_y, validation_data=(s_test_x, s_test_y), epochs=40, batch_size=50, verbose=2)
 
-
-plt.plot(model.history['acc'])
-plt.plot(model.history['val_acc'])
+'''
+plt.plot(mhistory.history['acc'])
+plt.plot(mhistory.history['val_acc'])
 plt.legend(['training', 'validation'], loc='lower right')
 plt.show()
 plt.savefig('train_history.png')
-
-
+'''
 
 pred_result = model.predict(test)
 pred_result = np.array(pred_result)
@@ -96,5 +94,15 @@ for i in pred_result:
 
 results = pd.Series(y_pred_final,name="Label")
 submission = pd.concat([pd.Series(range(1,28001),name = "ImageId"),results],axis = 1)
-submission.to_csv("submission.csv",index=False)
 
+submission.to_csv('submission.csv', index = False)
+'''
+from IPython.display import HTML
+def create_download_link(title = "Download CSV file", filename = "data.csv"):  
+    html = '<a href={filename}>{title}</a>'
+    html = html.format(title=title,filename=filename)
+    return HTML(html)
+
+# create a link to download the dataframe which was saved with .to_csv method
+create_download_link(filename='submission.csv')
+'''
